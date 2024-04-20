856be06490a2b72c9d629905b8db43342ffe63ff
Rellix999 <rellix_@hotmail.com>
2024-04-20 17:18:36 +0200

    Add php-cs-fixer

--- src/LaravelDismissibleServiceProvider.php
+++ src/LaravelDismissibleServiceProvider.php
@@ -9,7 +9,7 @@ class LaravelDismissibleServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
         ]);
     }
 }
